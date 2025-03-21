4d271497a4840075725c95ad877788b71efb3404
Ethan Astor <111445724+ethanast0@users.noreply.github.com>
2025-03-21 12:41:16 -0400

    Update client.ts

--- src/integrations/auth0/client.ts
+++ src/integrations/auth0/client.ts
@@ -1,8 +1,8 @@
 
 import { createAuth0Client, Auth0Client } from '@auth0/auth0-spa-js';
 
-const auth0Domain = 'your-auth0-domain.auth0.com'; // Replace with your Auth0 domain
-const auth0ClientId = 'your-auth0-client-id'; // Replace with your Auth0 client ID
+const auth0Domain = 'dev-thhqocp8jw018dpu.us.auth0.com'; // Replace with your Auth0 domain
+const auth0ClientId = '2yUjFaMgLB5GQcwuMRv8xEP9c7QiTJ5F'; // Replace with your Auth0 client ID
 
 export const getAuth0Client = async (): Promise<Auth0Client> => {
   return createAuth0Client({
