e732071ec392390425886dda1c086de0f081912b
barath <barat.paim@gmail.com>
2025-04-06 16:32:25 -0400

    test

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -15,7 +15,10 @@ const Header: React.FC<HeaderProps> = ({
   return (
     <header className={cn("py-6 px-8", className)}>
       <div className="container max-w-6xl mx-auto flex flex-col items-center relative">
-        <h1 className="text-2xl font-medium tracking-tight"></h1>
+        <h1 className="text-2xl font-medium tracking-tight">
+          <span className="text-zinc-500">deep</span>
+          <span className="text-green-500">type</span>
+        </h1>
         <p className={cn("text-monkey-subtle mt-1 text-center", isMobile ? "text-xs" : "text-sm")}>
           for those who know typing bends time
         </p>
